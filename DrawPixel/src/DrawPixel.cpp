6f378585e08f0fa2f992b0e13eb973cdde712777
tristanobe <emadavid961@gmail.com>
2025-03-03 22:43:16 +0100

    second commit

--- DrawPixel/src/DrawPixel.cpp
+++ DrawPixel/src/DrawPixel.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <fstream>
+#include <ostream>
 
+int main(int argc, char** argv) {
+    const int width = 600;
+    const int height = 600;
+    const int pixelCount = 3;
 
-int main(int argc, char** argv){
-    std::cout<< "hello world bonjour\n";
+    const int numPixels = width * height;
+    uint8_t* pixels = new uint8_t[numPixels * pixelCount];
+
+    for (int i = 0; i < numPixels * pixelCount; i++) {
+        pixels[i] = 0;
+    }
+
+    uint32_t* pixels2 = new uint32_t[numPixels];
+
+    for (int i = 0; i < numPixels; i++) {
+        uint32_t pixel = pixels2[i];
+        uint8_t r = 0;
+        uint8_t g = 0;
+        uint8_t b = 0;
+        pixels2[i] = r << 24 | g << 16 | b << 8;
+    }
+
+    
+    int centerX = 300;
+    int centerY = 300;
+    int index = centerY * width + centerX; 
+
+    uint8_t r = 255; 
+    uint8_t g = 255; 
+    uint8_t b = 255; 
+
+    pixels2[index] = r << 24 | g << 16 | b << 8; // Définir le pixel blanc
+
+    std::ofstream file("imgdevoir2.ppm");
+
+    file << "P3\n";
+    file << width << " " << height << "\n";
+    file << "255\n";
+
+    for (int i = 0; i < numPixels; i++) {
+        uint32_t pixel = pixels2[i];
+        uint8_t r = (pixel >> 24) & 255;
+        uint8_t g = (pixel >> 16) & 0xFF;
+        uint8_t b = (pixel >> 8) & 0xFF;
+        file << static_cast<int>(r) << " " << static_cast<int>(g) << " " << static_cast<int>(b) << " ";
+    }
+
+    file.close();
+
+    delete[] pixels;
+    delete[] pixels2;
+
     return 0;
 }
--- src/DrawPixel.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-
-int main(int argc, char** argv){
-    std::cout<< "hello world bonjour\n";
-    return 0;
-}
